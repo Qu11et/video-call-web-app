ca6f8b0ce2af6729e4368cad5f9dd8e7a291c911
Tai Khau <lekhauhuutai48@gmail.com>
2025-12-28 20:20:13 +0000

    Them pagination cho user profile

--- video-call-web-app/src/main/java/anhtaikhau/example/video_call_web_app/repository/ParticipantHistoryRepository.java
+++ video-call-web-app/src/main/java/anhtaikhau/example/video_call_web_app/repository/ParticipantHistoryRepository.java
@@ -1,7 +1,13 @@
 package anhtaikhau.example.video_call_web_app.repository;
 
 import anhtaikhau.example.video_call_web_app.model.ParticipantHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantHistoryRepository extends JpaRepository<ParticipantHistory, Long> {
+    
+    // Tìm theo User ID, sắp xếp và phân trang
+    // JPA tự động hiểu User_Id là tìm theo field id của object User
+    Page<ParticipantHistory> findByUser_Id(Long userId, Pageable pageable);
 }
