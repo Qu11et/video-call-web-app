b80e426291720444d3e0e5980214e99e5084a5b3
Tai Khau <lekhauhuutai48@gmail.com>
2026-01-17 09:13:02 +0000

    sua lai path cho docker compose

--- video-call-frontend/src/pages/LandingPage.tsx
+++ video-call-frontend/src/pages/LandingPage.tsx
@@ -112,7 +112,7 @@ export default function LandingPage() {
         )}
       </div>
       <div className="landing-card">
-        <h1>📹 Video Call App</h1>
+        <h1>Meeting App</h1>
         <p>Chọn chế độ gọi phù hợp</p>
 
         <div className="action-container">
